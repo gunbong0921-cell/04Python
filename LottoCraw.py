f95cf8f6de1189d7fbd473a70a94528be68cfea5
gunbong0921 <gunbong0921@gmail.com>
2026-06-10 18:30:16 +0900

    202606100630

---
+++ LottoCraw.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+url = 'https://www.dhlottery.co.kr/lt645/result'
+response = requests.get(url)
+
+if response.status_code==200:
+  html = response.text
+  soup = BeautifulSoup(html, 'html.parser')
+  title1_1 = soup.select_one('#swiperDiv > div.swiper-slide.swiper-slide-active > div > div.result-ballWrapper > div.result-ballBox')
+  print("추출1:", title1_1)
+
+
+else:
+  print("응답없음")
+  
+
+
